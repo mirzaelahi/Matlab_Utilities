af768be6176d558f50a4d56a7e6151ef9fdf8720
Mirza Mohammad Monzure Elahi <mirza.monzur@gmail.com>
2015-04-14 05:51:35 -0400

    Create Const.m

---
+++ Const.m
@@ -0,0 +1,11 @@
+classdef Const
+  % Consists universal physical constants
+    properties (Constant = true)
+        h = 6.63e-34;
+        eps0 = 8.854e-12;
+        c = 3e8;
+        k = 1.38e-23;
+        T = 300;
+        q = 1.6e-19;
+    end
+end
